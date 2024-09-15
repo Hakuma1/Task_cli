74d0c49a505da2ceadcca55f1f92c394e4376fa3
Hakuma1 <mc557164@gmail.com>
2024-09-15 16:21:40 +0100

    [UPD] Task manager CLI

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="task-cli",
     version="1.0",
+    author="MC",
     packages=find_packages(),
     entry_points={
         'console_scripts': [
